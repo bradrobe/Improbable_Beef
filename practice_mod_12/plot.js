8700d0d304dc91fe89f081f252a10d9e44518a63
Brad Roberts <bradroberts@Brads-MacBook-Air.local>
2022-03-13 20:29:38 -0400

    updating files in to git hub.

---
+++ practice_mod_12/plot.js
@@ -0,0 +1,37 @@
+//console.log(cityGrowths);
+
+
+//Sort the cities in descending order of population growth
+var sortedCities = cityGrowths.sort((a,b) =>
+a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
+
+console.log(sortedCities);
+
+
+//Select only the top five cities in terms of growth.
+var topFiveCities = sortedCities.slice(0,5);
+
+console.log(topFiveCities);
+
+// The parseInt converts strings into integers.
+var topFiveCityNames = cityGrowths.map(city => city.City);
+
+
+var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
+
+console.log(topFiveCityNames)
+console.log(topFiveCityGrowths)
+
+
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City" },
+    yaxis: {title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
